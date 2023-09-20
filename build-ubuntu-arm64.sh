d2c95579e5c4fe8827c25d0cdec0463289fbc9bc
pozasl <loic.pozas@wyca.fr>
2023-09-20 16:16:26 +0200

    [MOD] repo organisation fro ubuntu arm64

--- build-ubuntu-arm64.sh
+++ build-ubuntu-arm64.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #docker build -f docker/arm64/DockerFile -t ubuntu-arm64toolchain-wiringpi .
-docker build -f docker/arm64/DockerFile-Ubuntu-22.04 -t ubuntu-22.04-arm64-toolchain .
+docker build -f docker/arm64_ubuntu_22.04/DockerFile -t ubuntu-22.04-arm64-toolchain .
